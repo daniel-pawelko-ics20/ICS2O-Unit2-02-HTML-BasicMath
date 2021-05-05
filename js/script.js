9e1a413a8b00cd8a04a97b418da813542bcc7939
danielpawelkoic <>
2021-05-05 18:27:41 +0000

    redo

--- js/script.js
+++ js/script.js
@@ -6,9 +6,6 @@
 
 function doMathClicked () {
   // Math function
-  document.getElementById('add-math').innerHTML = '6 + 5 = ' + (6 + 5)
-  document.getElementById('subtract-math').innerHTML = '7 - 3 =  ' + (7 - 3)
-  document.getElementById('multiply-math').innerHTML = '3 + 4 * 2 = ' + (3 + 4 * 2)
-  document.getElementById('divide-math').innerHTML = '(4 / 2) + 3 = ' + ((4 / 2) + 3)
-  document.getElementById('exponent-math').innerHTML = '5 + 2 ** 3 = ' + (5 + 2 ** 3)
+  document.getElementById('area-math').innerHTML = 'Area of 5cm & 3cm =  ' + (5*3)
+  document.getElementById('perimiter-math').innerHTML = 'Perimiter of 5 & 3 =  ' + (2*(5+3))
 }
